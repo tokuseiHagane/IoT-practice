39d19b08218e44c3e3a0e4789dc1daf0982d8398
Константин Соломатин <kbc.off@list.ru>
2022-11-05 12:09:57 +0300

    Допиленные графики

---
+++ prac8/main.py
@@ -0,0 +1,79 @@
+import paho.mqtt.client as mqtt
+import xmltodict
+import json
+from datetime import datetime
+
+
+# Параметры подключения к MQTT-брокеру
+HOST = "192.168.2.25"  # IP чемодана
+PORT = 1883  # Стандартный порт подключения для Mosquitto
+KEEPALIVE = 60  # Время ожидания доставки сообщения, если при отправке оно будет прeвышено, брокер будет считаться недоступным
+
+# Словарь с топиками и собираемыми из них параметрами
+SUB_TOPICS = {
+    '/devices/wb-ms_11/controls/Illuminance': 'illuminance',
+    '/devices/wb-msw-v3_21/controls/CO2': 'co2',
+    '/devices/wb-msw-v3_21/controls/Sound Level': 'sound'
+}
+
+# Создание словаря для хранения данных JSON
+JSON_DICT = {}
+JSON_LIST = []
+
+
+def on_connect(client, userdata, flags, rc):
+    """ Функция, вызываемая при подключении к брокеру
+
+    Arguments:
+    client - Экземпляр класса Client, управляющий подключением к брокеру
+    userdata - Приватные данные пользователя, передаваемые при подключениии
+    flags - Флаги ответа, возвращаемые брокером
+    rc - Результат подключения, если 0, всё хорошо, в противном случае идем в документацию
+    """
+    print("Connected with result code " + str(rc))
+
+    # Подключение ко всем заданным выше топикам
+    for topic in SUB_TOPICS.keys():
+        client.subscribe(topic)
+
+
+def on_message(client: mqtt.Client, userdata, msg):
+    """ Функция, вызываемая при получении сообщения от брокера по одному из отслеживаемых топиков
+
+    Arguments:
+    client - Экземпляр класса Client, управляющий подключением к брокеру
+    userdata - Приватные данные пользователя, передаваемые при подключениии
+    msg - Сообщение, приходящее от брокера, со всей информацией
+    """
+    payload = msg.payload.decode()  # Основное значение, приходящее в сообщение, например, показатель температуры
+    topic = msg.topic  # Топик, из которого пришло сообщение, поскольку функция обрабатывает сообщения из всех топиков
+
+    param_name = SUB_TOPICS[topic]
+    JSON_DICT[param_name] = payload
+    JSON_DICT['time'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    JSON_LIST.append(JSON_DICT)
+    print(topic + " " + payload)
+
+    # Запись данных в файл
+    with open('../data.json', 'w') as file:
+        json_string = json.dumps({'data': JSON_LIST})  # Формирование строки JSON из словаря
+        file.write(json_string)
+    with open('../data.xml', 'w') as file:
+        unparsed = xmltodict.unparse({'data': JSON_LIST}, pretty=True)
+        file.write(unparsed)
+
+def main(subtopics=None):
+    SUB_TOPICS = SUB_TOPICS if None else subtopics
+    for value in SUB_TOPICS.values():
+        JSON_DICT[value] = 0
+    # Создание и настройка экземпляра класса Client для подключения в Mosquitto
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(HOST, PORT, KEEPALIVE)
+
+    client.loop_forever()  # Бесконечный внутренний цикл клиента в ожидании сообщений
+
+
+if __name__ == "__main__":
+    main()
---
+++ prac8/main_graphics.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import json
+
+
+def lineplot(x_data, y_data, x_label="", y_label="", title=""):
+    _, ax = plt.subplots()
+
+    ax.plot(x_data, y_data, lw = 2, color = '#539caf', alpha = 1)
+
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    plt.savefig('lineplot_co2.png')
+
+
+def histogram(data, n_bins, x_label = "", y_label = "", title = "", cumulative=False):
+    _, ax = plt.subplots()
+    ax.hist(data, bins=n_bins, cumulative=cumulative, color='#e28743')
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.set_title(title)
+    plt.savefig('histogram_illuminance.png')
+
+
+def circular(values, labels, title):
+    _, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct='%1.1f%%', shadow=True, wedgeprops={'lw':1, 'ls':'--','edgecolor':"k"}, rotatelabels=True)
+    ax.axis("equal")
+    ax.set_title(title)
+    plt.savefig('circular_vin.png')
+
+
+if __name__ == '__main__':
+    #  получение показателйей
+    dictionary = json.load(open('data.json', 'r'))["data"]
+    xAxis = [item['time'].split(' ')[-1] for item in dictionary]
+    yIlluminance = [int(item['illuminance']) for item in dictionary]
+    yVin = [float(item['Vin']) for item in dictionary]
+    yCO2 = [int(item['CO2']) for item in dictionary]
+
+    #  перевод показателей в целочисленное значение
+    xAxis = [sum([int(el.split(':')[::-1][n])*60**n for n in range(3)]) for el in xAxis]
+    base_t = xAxis[0]
+    xAxis = xAxis[100:]
+    yCO2 = yCO2[100:]
+    xAxis = [el % base_t for el in xAxis]
+
+    groups = {'23.0 В. >=': len([el for el in yVin if el >= 23.0 ]),
+              '< 23.0 В.': len(([el for el in yVin if el < 23.0 ]))}
+    
+    lineplot(xAxis, yCO2, 'Время, c', "Показатели", "Изменения показателя датчика СО2\n(первые 100 зн. забракованы)")
+    histogram(yIlluminance, 50, 'Кол-во', 'Значения', 'Распределение показателей СО2')
+    circular(groups.values(), groups.keys(), 'Кгруговая диаграмма Vin (~24В)')
+
